3fbc04ad673d8ba645c624581b2694f34c69d9e7
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2021-11-16 14:40:40 +0100

    chore: bump to 0.3.1

--- tflint-ruleset-aws-serverless/main.go
+++ tflint-ruleset-aws-serverless/main.go
@@ -10,7 +10,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "aws-serverless",
-			Version: "0.3.0",
+			Version: "0.3.1",
 			Rules:   rules.Rules,
 		},
 	})
